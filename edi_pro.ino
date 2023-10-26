6770fe5639deb4f21056bc0eba1d0885afaa508b
adkcodes27 <124795831+adkcodes27@users.noreply.github.com>
2023-10-26 14:20:48 +0530

    edi_pro.ino

--- edi_pro.ino
+++ edi_pro.ino
@@ -1,9 +1,20 @@
+
+const int dry = 630;//value for dry sensor
+const int wet = 460;//value for wet sensor
+
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(analogRead(A0));
+  int sensorVal = analogRead(A0);
+
+  int percentageHumidity = map(sensorVal, wet, dry, 100, 0);
+
+  Serial.print(percentageHumidity);
+  Serial.println("%"); 
+
   delay(1000);
 
 }
